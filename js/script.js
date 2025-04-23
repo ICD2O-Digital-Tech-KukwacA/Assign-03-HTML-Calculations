0313b5ba7731a90579542974e10a6533ca7477ec
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-23 01:31:29 +0000

    Assign-03-Calculations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates speed of light in different mediums.
+ */
+function calculate() {
+  // input
+    let n = parseFloat(document.getElementById('diamond').value);
+    let n2 = parseFloat(document.getElementById('water').value);
+    const c = 300000000
+  // process
+    let v = c / n;
+    let v2 = c / n2;
+
+  // output
+    document.getElementById('speed').innerHTML = 'The speed of light in diamond is ' + v.toExponential(2) + 'm/s' + '.'
+    document.getElementById('speed2').innerHTML = 'The speed of light in water is ' + v2.toExponential(2) + 'm/s' + '.'
+}
